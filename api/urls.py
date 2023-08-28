a1959896363d5897ae2a95625efa4c4eb18045ef
viper23 <htonnycarlos@gmail.com>
2023-08-28 16:57:55 +0200

    added restframework

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from api.views import getRoutes
+from api.views import get_routes, get_notes, get_note
 
 urlpatterns = [
-    path('', getRoutes, name='routes'),
+    path('', get_routes, name='routes'),
+    path('notes', get_notes, name='notes'),
+    path('note/<int:pk>', get_note, name='note'),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
-def getRoutes(request):
+@api_view(['GET'])
+def get_routes(request):
     routes = [
         {
             'Endpoint': '/notes/',
@@ -37,4 +42,17 @@ def getRoutes(request):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_note(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many = False)
+    return Response(serializer.data)
